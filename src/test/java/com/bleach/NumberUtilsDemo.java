93e5d3a8d614ec8f953c36e8c3fc2644ebfb4543
yuanlai <yuan_lai1234@163.com>
2020-08-04 18:57:31 +0800

    bleach-0.0.3, remove KafkaUtils

---
+++ src/test/java/com/bleach/NumberUtilsDemo.java
@@ -0,0 +1,28 @@
+package com.bleach;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @Author: xinput
+ * @Date: 2020-08-03 15:21
+ */
+public class NumberUtilsDemo {
+
+    @Test
+    public void test01() {
+        double aa = -19162431.1254;
+        String a = "-19162431.1254";
+        String b = "-19162431a1254";
+        String c = "中文";
+        System.out.println(NumberUtils.isNumeric(Double.toString(aa)));
+        System.out.println(NumberUtils.isNumeric(a));
+        System.out.println(NumberUtils.isNumeric(b));
+        System.out.println(NumberUtils.isNumeric(c));
+
+        Assert.assertEquals(NumberUtils.isNumeric(Double.toString(aa)), true);
+        Assert.assertEquals(NumberUtils.isNumeric(a), true);
+        Assert.assertEquals(NumberUtils.isNumeric(b), false);
+        Assert.assertEquals(NumberUtils.isNumeric(c), false);
+    }
+}
