fadfb074d29c21e00c4f2125eb48c66179980de9
im.xinput <Yuan,.?@1234>
2021-01-26 16:45:21 +0800

    doc: 添加注释

--- src/test/java/com/xinput/bleach/LocalDateUtilsDemo.java
+++ src/test/java/com/xinput/bleach/LocalDateUtilsDemo.java
@@ -13,33 +13,41 @@ import java.util.List;
  */
 public class LocalDateUtilsDemo {
 
-    @Test
-    public void format() {
-        LocalDate localDate = LocalDate.now();
-        System.out.println(LocalDateUtils.formatDate(localDate));
-        System.out.println(LocalDateUtils.format(localDate, DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+  @Test
+  public void format() {
+    LocalDate localDate = LocalDate.now();
+    System.out.println(LocalDateUtils.formatDate(localDate));
+    System.out.println(LocalDateUtils.format(localDate, DateTimeFormatter.ofPattern("yyyy-MM-dd")));
 
-        System.out.println(LocalDateUtils.asMillSecond(System.currentTimeMillis()));
-        System.out.println(LocalDateUtils.asSecond(System.currentTimeMillis() / 1000L));
+    System.out.println(LocalDateUtils.asMillSecond(System.currentTimeMillis()));
+    System.out.println(LocalDateUtils.asSecond(System.currentTimeMillis() / 1000L));
+  }
+
+  @Test
+  public void parse() {
+    System.out.println(LocalDateUtils.parseDate("2020-09-03"));
+    final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    System.out.println(LocalDateUtils.parse("2020-09-03 10:20:30", DATE_FORMATTER));
+  }
+
+  @Test
+  public void getMiddleLocalDate() {
+    LocalDate begin = LocalDate.of(2019, 12, 31);
+    LocalDate end = LocalDate.of(2020, 12, 1);
+
+    List<LocalDate> middleLocalDates = LocalDateUtils.getMiddleLocalDate(begin, end);
+    for (LocalDate middleLocalDate : middleLocalDates) {
+      System.out.println(middleLocalDate.toString());
     }
 
-    @Test
-    public void parse() {
-        System.out.println(LocalDateUtils.parseDate("2020-09-03"));
-        final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        System.out.println(LocalDateUtils.parse("2020-09-03 10:20:30", DATE_FORMATTER));
-    }
+    System.out.println(begin.atStartOfDay());
+  }
 
-    @Test
-    public void getMiddleLocalDate() {
-        LocalDate begin = LocalDate.of(2019, 12, 10);
-        LocalDate end = LocalDate.of(2020, 1, 10);
-
-        List<LocalDate> middleLocalDates = LocalDateUtils.getMiddleLocalDate(begin, end);
-        for (LocalDate middleLocalDate : middleLocalDates) {
-            System.out.println(middleLocalDate.toString());
-        }
-
-        System.out.println(begin.atStartOfDay());
-    }
+  @Test
+  public void test() {
+    LocalDate date1 = null;
+    LocalDate date2 = LocalDate.now();
+    System.out.println(LocalDateUtils.isEqual(date1, date2));
+    System.out.println(LocalDateUtils.isEqual(date2, date1));
+  }
 }
